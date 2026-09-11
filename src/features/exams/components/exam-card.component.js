823ad3c60a3c797c5c295a2f69e81454e469a99e
Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2026-09-11 04:25:17 +0300

    feat: export getExamStatusInfo from exam-status-badge.component and import it into exam-card.component

--- src/features/exams/components/exam-card.component.js
+++ src/features/exams/components/exam-card.component.js
@@ -4,7 +4,7 @@ import { renderCard } from "../../../shared/components/Card/card.component.js";
 import { renderBadge } from "../../../shared/components/Badge/badge.component.js";
 import { renderButton } from "../../../shared/components/Button/button.component.js";
 import { formatDate } from "../../../shared/utils/date.utils.js";
-import { renderExamStatusBadge } from "./exam-status-badge.component.js";
+import { renderExamStatusBadge, getExamStatusInfo } from "./exam-status-badge.component.js";
 
 /**
  * Returns HTML string for student exam card.
